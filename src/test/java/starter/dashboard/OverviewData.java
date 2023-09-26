e6553877d984b2031d4d0fac41b5191b558a86ad
Francisco Cabezas <frank_vega25@hotmail.com>
2023-09-25 21:06:08 -0500

    add Questions

---
+++ src/test/java/starter/dashboard/OverviewData.java
@@ -0,0 +1,16 @@
+package starter.dashboard;
+
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.questions.TextContent;
+
+public class OverviewData {
+
+    public static Question<String> creditAvailable() {
+        return actor -> TextContent.of(FinancialOverview.CREDIT_AVAILABLE).answeredBy(actor);
+    }
+
+    public static Question<String> totalBalance() {
+        return actor -> TextContent.of(FinancialOverview.TOTAL_BALANCE).answeredBy(actor);
+    }
+
+}
--- src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/LoginStepDefinitions.java
@@ -4,8 +4,11 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.serenitybdd.screenplay.Actor;
+import starter.dashboard.OverviewData;
 import starter.login.DoLogin;
 import starter.navigation.NavigateTo;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class LoginStepDefinitions {
 
@@ -24,7 +27,8 @@ public class LoginStepDefinitions {
 
     @Then("he should have access to manage his account")
     public void he_should_have_access_to_manage_his_account() {
-
+        System.out.println("**********" + OverviewData.creditAvailable().answeredBy(theActorInTheSpotlight()));
+        System.out.println("**********" + OverviewData.totalBalance().answeredBy(theActorInTheSpotlight()));
     }
 
 }
