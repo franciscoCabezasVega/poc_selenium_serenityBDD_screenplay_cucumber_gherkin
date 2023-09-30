c87b773c829f7589b85c19cb13045f6ef8227695
Francisco Cabezas <frank_vega25@hotmail.com>
2023-09-30 17:55:08 -0500

    add assertions conf and fixed report

--- src/test/java/starter/dashboard/OverviewData.java
+++ src/test/java/starter/dashboard/OverviewData.java
@@ -6,11 +6,11 @@ import net.serenitybdd.screenplay.questions.TextContent;
 public class OverviewData {
 
     public static Question<String> creditAvailable() {
-        return actor -> TextContent.of(FinancialOverview.CREDIT_AVAILABLE).answeredBy(actor);
+        return actor -> TextContent.of(FinancialOverview.CREDIT_AVAILABLE).answeredBy(actor).trim();
     }
 
     public static Question<String> totalBalance() {
-        return actor -> TextContent.of(FinancialOverview.TOTAL_BALANCE).answeredBy(actor);
+        return actor -> TextContent.of(FinancialOverview.TOTAL_BALANCE).answeredBy(actor).trim();
     }
 
 }
